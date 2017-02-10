3690dbf1dae7551c6f10526c8733fbcdb7c195b9
samuel michaux <kameleon836@gmail.com>
2017-02-10 17:09:33 +0100

    Add new Tray - More change

--- appFront/main.js
+++ appFront/main.js
@@ -2,20 +2,24 @@
 //handle setupevents as quickly as possible
 const setupEvents = require('./installers/setupEvents')
 if (setupEvents.handleSquirrelEvent()) {
-   // squirrel event handled and app will exit in 1000ms, so don't do anything else
-   return;
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
 }
 
-const {app, BrowserWindow,Menu, electron} = require('electron')
+const electron = require('electron')
+const {app, BrowserWindow,Menu} = electron
 const path = require('path')
 const url = require('url')
-const Tray = require('electron').Tray
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+const Tray = electron.Tray
+const AutoLaunch = require('auto-launch');
+
 let win
+let tray
 
 function createWindow () {
-
+    const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
+    const mW = width - 430;
+    const mH = height - 230;
     // Create the browser window.
     win = new BrowserWindow({
         width: 400,
@@ -24,21 +28,26 @@ function createWindow () {
         height: 150,
         maxHeight: 250,
         minHeight: 150,
+        x: mW,
+        y: mH,
         icon: 'assets/img/sokys.png',
         title: 'Traffic Speed',
         maximized: false,
         transparent: true,
+        alwaysOnTop: true,
         frame: false
     });
 
     // and load the index.html of the app.
+
+
     win.loadURL(url.format({
         pathname: path.join(__dirname,'html', 'index.html'),
         protocol: 'file:',
         slashes: true
     }))
 
-    tray()
+    trayOn()
 
     // Open the DevTools.
     // win.webContents.openDevTools()
@@ -75,31 +84,85 @@ app.on('activate', () => {
     }
 })
 
-function tray(){
-    const nativeImage = require('electron').nativeImage
+function trayOn(){
+    const nativeImage = electron.nativeImage
     let image = nativeImage.createFromPath(path.join(__dirname,'assets', 'img', 'sokys.png'))
-    const tray = new Tray(image);
+    tray = new Tray(image);
     // Petit bonus : on affiche une bulle au survol.
+
+    var trafficSpeedAutoLauncher = new AutoLaunch({
+        name: 'Traffic\ Speed',
+        path: path.join(__dirname,'Traffic\ Speed.exe'),
+    });
+    // var updateAutoLauncher = new AutoLaunch({
+    //     name: 'Traffic\ Speed\ Updater',
+    //     path: path.join(__dirname,'Update.exe'),
+    // });
 
     tray.on('click', () => {
         win.isVisible() ? win.hide() : win.show()
     })
+
     const contextMenu = Menu.buildFromTemplate([
         {
             label: 'Ouvrir owncloud',
             click: () => {
                 // cette méthode permet d’ouvrir une URL dans le navigateur par défaut
-                 require('electron').shell.openExternal("http://mickey06.ddns.net")
+                require('electron').shell.openExternal("http://mickey06.ddns.net")
             }
+        }, {
+            type: 'separator'
+        },
+        {
+            label: 'Au démarrage',
+            type: 'checkbox',
+            click: (e) => {
+                if (e.checked){
+                    // updateAutoLauncher.enable();
+                    trafficSpeedAutoLauncher.enable();
+                }else{
+                    // updateAutoLauncher.disable();
+                    trafficSpeedAutoLauncher.disable();
+                }
+            }
+        }, {
+            type: 'separator'
+        },
+        {
+            label: 'Toujours devant',
+            type: 'checkbox',
+            click: (e) => {
+                if (e.checked){
+                    win.setAlwaysOnTop(true)
+                }else{
+                    win.setAlwaysOnTop(false)
+                }
+            }
+        }, {
+            type: 'separator'
         },
         {
             label: 'Quitter',
             click: () => {
                 // cette méthode permet d’ouvrir une URL dans le navigateur par défaut
-                 app.quit()
+                app.quit()
             }
         }
     ])
+
+    trafficSpeedAutoLauncher.isEnabled()
+    .then(function(isEnabled){
+        if(isEnabled){
+            contextMenu.items[2].checked = true
+            return isEnabled;
+        }
+        return isEnabled
+    })
+
+    if (win.isAlwaysOnTop()){
+        contextMenu.items[4].checked = true
+    }
+
     tray.setToolTip('Traffic Speed');
     tray.setContextMenu(contextMenu)
 }
